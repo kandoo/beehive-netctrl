1ef1e27fb5a0da1bd312e5c4c7258bf36cb7ac99
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2014-09-27 23:48:12 -0400

    Enhances packet message in NOM

--- nom/packet.go
+++ nom/packet.go
@@ -1,19 +1,30 @@
 package nom
 
+// Special ports.
+const (
+	PortFlood UID = "Ports.PortBcast"
+	PortAll   UID = "Ports.PortAll"
+)
+
 // PacketIn messages are emitted when a packet is forwarded to the controller.
 type PacketIn struct {
-	Node   UID
-	InPort UID
-	Packet Packet
+	Node     UID
+	InPort   UID
+	BufferID PacketBufferID
+	Packet   Packet
 }
 
 // PacketOut messages are emitted to send a packet out of a port.
 type PacketOut struct {
-	Node    UID
-	OutPort UID
-	InPort  UID
-	Packet  Packet
+	Node     UID
+	OutPort  UID
+	InPort   UID
+	BufferID PacketBufferID
+	Packet   Packet
 }
 
 // Packet is simply the packet data.
 type Packet []byte
+
+// PacketBufferID represents a packet buffered in the switch.
+type PacketBufferID uint32
