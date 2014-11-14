55409a67665933220ea7a9dd750bf2d4c6a2a9b0
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2014-11-14 11:28:13 -0500

    Add delTriggerHandler to controller

--- controller/trigger.go
+++ controller/trigger.go
@@ -10,11 +10,27 @@ type addTriggerHandler struct{}
 func (h addTriggerHandler) Rcv(msg bh.Msg, ctx bh.RcvContext) error {
 	add := msg.Data().(nom.AddTrigger)
 	var nt nodeTriggers
-	ctx.Dict(triggersDict).GetGob(string(add.Node), &nt)
+	dict := ctx.Dict(triggersDict)
+	dict.GetGob(string(add.Node), &nt)
 	nt.maybeAddTrigger(nom.Trigger(add))
-	return ctx.Dict(triggersDict).PutGob(string(add.Node), &nt)
+	return dict.PutGob(string(add.Node), &nt)
 }
 
 func (h addTriggerHandler) Map(msg bh.Msg, ctx bh.MapContext) bh.MappedCells {
 	return nodeDriversMap(msg.Data().(nom.AddTrigger).Node)
+}
+
+type delTriggerHandler struct{}
+
+func (h delTriggerHandler) Rcv(msg bh.Msg, ctx bh.RcvContext) error {
+	del := msg.Data().(nom.DelTrigger)
+	var nt nodeTriggers
+	dict := ctx.Dict(triggersDict)
+	dict.GetGob(string(del.Node), &nt)
+	nt.delTrigger(nom.Trigger(del))
+	return dict.PutGob(string(del.Node), &nt)
+}
+
+func (h delTriggerHandler) Map(msg bh.Msg, ctx bh.RcvContext) bh.MappedCells {
+	return nodeDriversMap(msg.Data().(nom.DelTrigger).Node)
 }
